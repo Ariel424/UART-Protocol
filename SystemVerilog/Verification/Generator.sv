1ebbc68534422945c106c73cab93302cafd24daa
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2025-12-02 09:08:12 +0200

    Create Generator.sv

---
+++ SystemVerilog/Verification/Generator.sv
@@ -0,0 +1,31 @@
+class generator;
+
+  transaction tr;
+  mailbox #(transaction) mbx;
+
+  event done;
+  event drvnext;
+  event sconext;
+
+  int count = 0;
+
+  function new(mailbox #(transaction) mbx);
+    this.mbx = mbx;
+    tr = new();
+  endfunction
+
+  task run();
+    repeat (count) begin
+      assert(tr.randomize) else $error("[GEN] : Randomization Failed");
+      
+      mbx.put(tr.copy);
+      $display("[GEN]: Oper: %0s   Din: %0d", tr.oper.name(), tr.dintx);
+
+      @(drvnext);
+      @(sconext);
+    end
+
+    ->done;
+  endtask
+
+endclass
